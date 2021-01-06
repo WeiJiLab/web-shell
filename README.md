912057115bf8af75bcc721ad68b6e7410397e5c8
xpw-osr <64068064+xpw-osr@users.noreply.github.com>
2021-01-06 14:20:32 +0800

    add base code

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # web-shell
 Microfrontends shell with Piral
+
+
+## Build
+```bash
+$ piral build
+```
+
+## Debug
+```bash
+$ piral debug
+```
+
+## Note
+The url in code should be replaced to real pilet service. 
+Currently, we use local 'sample-pilet-service' for developing.
+```typescript
+const feedUrl = 'http://localhost:9000/api/v1/pilet';
+```
+
+**To install 'sample-pilet-service'**
+```bash
+$ npm install sample-pilet-service -g
+```
+
+**Launch 'sample-pilet-service'**
+```bash
+$ sample-pilet-service
+```
---
+++ src/layout.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import { ComponentsState, ErrorComponentsState, Menu, Notifications, SwitchErrorInfo, MenuItemProps } from 'piral';
+import { Link } from 'react-router-dom';
+
+const MenuItem: React.FC<MenuItemProps> = ({ children }) => <li className="nav-item">{children}</li>;
+
+const defaultTiles = (
+  <>
+  </>
+);
+
+const defaultMenuItems = (
+  <>
+    <MenuItem type="general" meta={{}}>
+      <Link className="nav-link text-dark" to="/not-found">
+        Not Found
+      </Link>
+    </MenuItem>
+  </>
+);
+
+export const errors: Partial<ErrorComponentsState> = {
+  not_found: () => (
+    <div>
+      <p className="error">Could not find the requested page. Are you sure it exists?</p>
+      <p>
+        Go back <Link to="/">to the dashboard</Link>.
+      </p>
+    </div>
+  ),
+};
+
+export const layout: Partial<ComponentsState> = {
+  ErrorInfo: props => (
+    <div>
+      <h1>Error</h1>
+      <SwitchErrorInfo {...props} />
+    </div>
+  ),
+  DashboardContainer: ({ children }) => (
+    <div>
+      <h1>Hello, Security!</h1>
+      <p>Welcome to Security Portal</p>
+      <div className="tiles">
+        {defaultTiles}
+        {children}
+      </div>
+    </div>
+  ),
+  DashboardTile: ({ columns, rows, children }) => <div className={`tile cols-${columns} rows-${rows}`}>{children}</div>,
+  Layout: ({ children }) => (
+    <div>
+      <Notifications />
+      <Menu type="general" />
+      <div className="container">{children}</div>
+    </div>
+  ),
+  MenuContainer: ({ children }) => {
+    const [collapsed, setCollapsed] = React.useState(true);
+    return (
+      <header>
+        <nav className="navbar navbar-light navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              Security Portal
+            </Link>
+            <button
+              aria-label="Toggle navigation"
+              type="button"
+              onClick={() => setCollapsed(!collapsed)}
+              className="navbar-toggler mr-2">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className={`collapse navbar-collapse d-sm-inline-flex flex-sm-row-reverse ${collapsed ? '' : 'show'}`}
+              aria-expanded={!collapsed}>
+              <ul className="navbar-nav flex-grow">
+                {children}
+                {defaultMenuItems}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  },
+  MenuItem,
+  NotificationsHost: ({ children }) => <div className="notifications">{children}</div>,
+  NotificationsToast: ({ options, onClose, children }) => (
+    <div className={`notification-toast ${options.type}`}>
+      <div className="notification-toast-details">
+        {options.title && <div className="notification-toast-title">{options.title}</div>}
+        <div className="notification-toast-description">{children}</div>
+      </div>
+      <div className="notification-toast-close" onClick={onClose} />
+    </div>
+  ),
+};
